4253381da919d2b2f7f29e1cf6fe033c51b9829c
borisreyson <borisreyson@outlook.com>
2022-11-04 18:31:23 +0800

    Update new Project

---
+++ android/app/src/main/kotlin/com/misit/absen_io_abp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.misit.absen_io_abp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/misit/faceidplus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.misit.faceidplus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
